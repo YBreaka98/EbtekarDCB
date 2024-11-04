0c8b10dffc9998ec69f86369a6d74933938a745b
Yousef Breaka <46008548+YBreaka98@users.noreply.github.com>
2024-11-04 20:18:18 +0200

    .

--- src/Interfaces/EbtekarInterface.php
+++ src/Interfaces/EbtekarInterface.php
@@ -2,8 +2,10 @@
 
 namespace Ybreaka98\EbtekarDCB\Interfaces;
 
+use Ybreaka98\EbtekarDCB\Responses\ConfirmLoginResponse;
 use Ybreaka98\EbtekarDCB\Responses\EbtekarResponse;
 use Ybreaka98\EbtekarDCB\Responses\ProtectedScriptResponse;
+use Ybreaka98\EbtekarDCB\Responses\SubscriptionDetailsResponse;
 
 interface EbtekarInterface
 {
@@ -11,13 +13,13 @@ interface EbtekarInterface
 
     public function login(string $msisdn, string $transaction_identify, string $device_type = 'android'): EbtekarResponse;
 
-    public function confirmLogin(string $msisdn, string $otp, string $device_type = 'android'): EbtekarResponse;
+    public function confirmLogin(string $msisdn, string $otp, string $device_type = 'android'): ConfirmLoginResponse;
 
     public function upgrade(string $msisdn, string $uuid): EbtekarResponse;
 
     public function upgradeConfirm(string $msisdn, string $otp, string $uuid, $device_token = 'android'): EbtekarResponse;
 
-    public function subscriptionDetails(string $msisdn): EbtekarResponse;
+    public function subscriptionDetails(string $msisdn): SubscriptionDetailsResponse;
 
     public function directUnsubscribe(string $msisdn): EbtekarResponse;
 
